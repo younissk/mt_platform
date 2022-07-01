0ae2350884f39441d9a92668d8c4d65fa04214f6
younissk <younisskandah@gmail.com>
2022-07-01 14:54:05 +0200

    started adding login functionality

--- src/App.svelte
+++ src/App.svelte
@@ -1,77 +1,11 @@
 <script>
-  import TopNav from "./features/navigation/TopNav.svelte";
-  import LeftNav from "./features/navigation/LeftNav.svelte";
-  import RightNav from "./features/navigation/RightNav.svelte";
-  import CommunityPage from "./features/community/CommunityPage.svelte";
-  import AddPostPage from "./features/add-post/AddPostPage.svelte";
-  import { Router, Route } from "svelte-routing";
-  import MobileNav from "./features/navigation/components/MobileNav.svelte";
+  import MainApp from "./MainApp.svelte";
+  import Login from "./Login.svelte";
+  import { UserIdStore } from "./stores";
 </script>
 
-<Router>
-  <main>
-    <div class="right">
-      <TopNav pageName="Community" />
-      <div class="content">
-        <Route path="/">
-          <CommunityPage />
-          <!-- <AddPostPage /> -->
-        </Route>
-        <Route path="add-post">
-          <AddPostPage />
-        </Route>
-      </div>
-      <RightNav />
-    </div>
-    <div class="left">
-      <LeftNav />
-    </div>
-    <MobileNav />
-  </main>
-</Router>
-
-<style>
-  .content {
-    overflow-y: auto;
-    margin: 80px 40px;
-  }
-
-  main {
-    display: flex;
-    height: 100%;
-    width: 100%;
-  }
-
-  .left {
-    position: fixed;
-    width: 200px;
-    height: 100%;
-    top: 0;
-    left: 0;
-    overflow-x: hidden;
-    overflow-y: auto;
-  }
-
-  .right {
-    position: absolute;
-    top: 0;
-    right: 200px;
-    left: 200px;
-  }
-
-  @media (max-width: 1000px) {
-    .right {
-      right: 0;
-    }
-  }
-
-  @media (max-width: 600px) {
-    .right {
-      right: 0;
-      left: 0;
-    }
-    .left {
-      display: none;
-    }
-  }
-</style>
+{#if $UserIdStore === ""}
+  <Login />
+{:else}
+  <MainApp />
+{/if}
