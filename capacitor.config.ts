4f3775d12b95cd6f5ea7253a751df22b3d715342
younissk <younisskandah@gmail.com>
2022-07-08 18:14:54 +0200

    capacitor config

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.mytazkiyah.platform',
+  appName: 'MyTazkiyah',
+  webDir: 'public',
+  bundledWebRuntime: false
+};
+
+export default config;
